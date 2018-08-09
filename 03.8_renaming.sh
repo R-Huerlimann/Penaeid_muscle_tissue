3bd6961a9f07bac9133d400a4faafaa329ad7262
R-Huerlimann <R-Huerlimann@users.noreply.github.com>
2018-08-10 08:56:19 +1000

    Update 03.8_renaming.sh

--- 03.8_renaming.sh
+++ 03.8_renaming.sh
@@ -8,6 +8,6 @@ species_list=( Pmono Plong Mensi Plati Psemi Mende Pescu Fmerg Lvana )
 ### Adding species IDs to contig IDs ###
 for i in ${species_list[@]}; do
        echo "renaming $i"
-       sed -i.bak "s|contig|${i}_contig|" 04_Annotation/${i}_final.fasta
+       sed -i.bak "s|contig|${i}_contig|" 04_Annotation/${i}_final_raw.fasta
        echo "done"
 done
